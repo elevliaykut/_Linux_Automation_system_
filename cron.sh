a21b95cc9edbc7dfb062751aa9d2691ad0efa607
elevliaykut <elevli.aykut.28@gmail.com>
2020-01-23 12:12:31 +0300

    First Commit

---
+++ cron.sh
@@ -0,0 +1,3 @@
+00 09 * * *    /home/oracle/_Linux_Automation_system_/check.sh
+02 09 * * * sh /home/oracle/_Linux_Automation_system_/mail.sh
+05 09 * * * rm /home/oracle/_Linux_Automation_system_/report.html
