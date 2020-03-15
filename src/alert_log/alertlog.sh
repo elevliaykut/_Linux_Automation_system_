d5c9420deb78c12a40249573f6c00af5063aad20
elevliaykut <elevli.aykut.28@gmail.com>
2020-03-15 23:56:39 +0300

    Refactored Alertlog path

--- src/alert_log/alertlog.sh
+++ src/alert_log/alertlog.sh
@@ -5,7 +5,7 @@ echo " "
 echo " "
 echo " "
 echo " "
-orahatasi=`tail -n 500 /u01/diag/rdbms/prod/PROD/trace/alert_PROD.log |grep "ORA-" |wc -l`
+orahatasi=`tail -n 500 #! Your Alertlog Path  |grep "ORA-" |wc -l`
 
 if [ $orahatasi -eq 0 ]
 then 
